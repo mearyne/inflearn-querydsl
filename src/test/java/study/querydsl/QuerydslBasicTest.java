f45d1ed8619a834b9111a654e0911b1d8d462cba
ljm <mearyne@gmail.com>
2022-09-30 15:07:56 +0900

    test querydsl 기본문법 v3 끝

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -537,5 +537,72 @@ public class QuerydslBasicTest {
         return usernameCond == null ? null : member.username.eq(usernameCond);
     }
 
+    @Test
+    public void buikUpdate() {
+        long count = queryFactory
+                .update(member)
+                .set(member.username, "비회원")
+                .where(member.age.lt(28))
+                .execute();
+
+        em.flush();
+        em.clear();
+
+        List<Member> findMembers = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        for (Member findMember : findMembers) {
+            System.out.println("findMember = " + findMember);
+        }
+
+    }
+
+    @Test
+    public void bulkAdd() {
+        long count = queryFactory
+                .update(member)
+                .set(member.age, member.age.multiply(1))
+                .execute();
+    }
+
+    @Test
+    public void bulkDelete() {
+        long count = queryFactory
+                .delete(member)
+                .where(member.age.gt(18))
+                .execute();
+    }
+
+    @Test
+    public void sqlFunction() {
+
+        List<String> result = queryFactory
+                .select(Expressions.stringTemplate("function('replace', {0}, {1}, {2})"
+                        , member.username, "member", "M")) // -> member를 M으로 수정한다
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void sqlFunction2() {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+//                .where(member.username
+//                        .eq(Expressions.stringTemplate("function('lower', {0})"
+//                                , member.username)))
+                .where(member.username.eq(member.username.lower()))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+
+        }
+    }
 
 }
